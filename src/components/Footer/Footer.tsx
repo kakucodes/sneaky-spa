4c63201947fbb815a34061e73d8be6a0f52f485e
Obie Kaku <obiekaku@gmail.com>
2024-12-29 15:07:30 -0600

    updated footer links

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -8,20 +8,21 @@ export const Footer = () => {
           <div className="mx-2">
             <AudioPlayer />
           </div>
-          <div>
+          <div
+            style={{
+              background:
+                "radial-gradient(circle at right center, rgba(255,240,207,1) 0%, rgba(250,224,191,1) 51%, rgba(168,144,112,1) 100%)",
+            }}
+            className="btn border-black"
+          >
             <a
-              style={{
-                background:
-                  "radial-gradient(circle at right center, rgba(255,240,207,1) 0%, rgba(168,144,112,1) 100%)",
-              }}
               href="https://x.com/SneakyProds"
               target="_blank"
               rel="noreferrer"
-              className="btn border-black"
             >
               <img
-                height={50}
-                width={50}
+                height={35}
+                width={35}
                 src={`${process.env.PUBLIC_URL}/x-logo.png`}
                 alt="X logo"
                 className="img-fluid"
@@ -29,18 +30,14 @@ export const Footer = () => {
             </a>
 
             <a
-              style={{
-                background:
-                  "radial-gradient(circle at right center, rgba(255,240,207,1) 0%, rgba(250,224,191,1) 51%, rgba(168,144,112,1) 100%)",
-              }}
               href="https://discord.com/invite/PF52wHyDe4"
               target="_blank"
               rel="noreferrer"
-              className="btn ms-2 border-black"
+              className="d-flex align-items-center"
             >
               <img
-                height={50}
-                width={50}
+                height={35}
+                width={35}
                 src={`${process.env.PUBLIC_URL}/discord-logo.png`}
                 alt="Discord logo"
                 className="img-fluid"
